e7d7bd576b3c9952991cc96aa8f2ab286a550d12
Duvey <rudow.david@gmail.com>
2021-04-04 21:12:18 -0700

    added grid generating function

---
+++ Hexagame/Assets/Scripts/GridController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public int gridSize = 5;
+    public GameObject gridHexagonPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+        Debug.Log("Program Started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateGrid()
+    {
+        Debug.Log("Generating Grid");
+        int[] grid;
+        for (int x = -gridSize; x < gridSize; x++) 
+        {
+            for (int y = -gridSize; y < gridSize; y++) 
+            {
+                for (int z = -gridSize; z < gridSize; z++) 
+                {
+                    if (x+y+z == 0)
+                    {
+                        Debug.Log($"Element added at: {x},{y},{z}");
+                        if(x==0 & y == 0 & x == 0)
+                        {
+                            Instantiate(gridHexagonPrefab, new Vector2(0,0), Quaternion.identity);
+                        }
+
+                        else
+                        {
+                            float centX = 1 * (Convert.ToSingle(Math.Sqrt(3)) * Convert.ToSingle(x) + Convert.ToSingle(Math.Sqrt(3) / 2) * Convert.ToSingle(z));
+                            float centY = 1 * (Convert.ToSingle(3.0 / 2) * Convert.ToSingle(z));
+                            Instantiate(gridHexagonPrefab, new Vector2(centX,centY), Quaternion.identity);
+                            
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+    // public void DrawGrid()
+    // {
+
+    // # need the . in 3/2 to make 3.0/2
+    //     if (self.x == 0 and self.y == 0 and self.z == 0):
+    //         self.centx = self.canvas_width / 2
+    //         self.centy = self.canvas_height / 2
+    //         print("one")
+    //     else:
+    //         print("two")
+    //         if self.rot == 'flat':
+    //             self.centx = (self.size * ((3.0 / 2) * self.x)) + (self.canvas_width / 2)
+    //             self.centy = (self.size * ((math.sqrt(3) / 2) * self.x + math.sqrt(3) * self.z)) + (self.canvas_height / 2)
+    //         elif self.rot == 'pointy':
+    //             self.centx = (self.size * (math.sqrt(3) * self.x + math.sqrt(3) / 2 * self.z)) + (self.canvas_height / 2)
+    //             self.centy = (self.size * ((3.0 / 2) * self.z)) + (self.canvas_width / 2)    
+    // }
+
